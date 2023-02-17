36035091b85e3eda2927422a38b664eba9f43c8c
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-02-17 12:56:06 -0800

    idiot child learns to type part 4

--- Course_Project___Phase_3.py
+++ Course_Project___Phase_3.py
@@ -59,7 +59,7 @@ def printinfo(DetailsPrinted):
     TotNetPay = 0.00
 #****************************************************************************************************************************
     # write the line of code that will open the file in read mode and assign it to EmpFile (Hint: see week 6, lab 2 as a guide)
-    with open(Employees.txt, "r") as EmpFile:    
+    with open(FILENAME, "r") as EmpFile:    
         while True:
             rundate = input ("Enter start date for report (MM/DD/YYYY) or All for all data in file: ")
             if (rundate.upper() == "ALL"):
@@ -80,7 +80,7 @@ def printinfo(DetailsPrinted):
             EmpDetail = EmpDetail.replace("\n", "")
                         
             # write the ine of code that will split EmpDetail on the pipe delimiter and assign to the list EmpList
-            EmpList = EmpDetails.split("|")
+            EmpList = EmpDetail.split("|")
 
 #********************************************************************************************************************************
             fromdate = EmpList[0]
@@ -131,7 +131,7 @@ def PrintTotals(EmpTotals):
 #***************************************************************************************************************************************
 if __name__ == "__main__":
         # write the line of code that will open the file in append mode and assign it to EmpFile (Hint: see week 6, lab 2 as a guide)
-        with open(Employees.txt, "a") as EmpFile:
+        with open(FILENAME, "a") as EmpFile:
 
             #EmpDetailList = []
             EmpTotals = {}
@@ -148,17 +148,11 @@ if __name__ == "__main__":
                 todate = todate.strftime('%Y-%m-%d')
                 # write the line of code that will assign to EmpDetail a pipe delimited string of fromdate, todate, empname, hours, hourlyrate and taxrate and a carriage return at the end
                 EmpDetail = fromdate + "|" + todate +"|" + empname + "|" + str(hours) + "|" + str(hourlyrate) + "|" + str(taxrate) + "\n"
-
                 # write the line of code that will write EmpDetail to the file
-                EmpFile.write(EmpDetail)
-
-                
-                
-                
+                EmpFile.write(EmpDetail)             
             # close file to save data
             # write the line of code that will close the file
-            EmpFile.close()
-                           
+            EmpFile.close()                           
             printinfo(DetailsPrinted)
 
 #***********************************************************************************************************************************************
